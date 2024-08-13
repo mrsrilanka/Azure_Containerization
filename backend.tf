950cc9775cac4d9b85250d1db173b4e968575ee6
Naninga Karunaratne <naninga.karu@gmail.com>
2024-08-13 14:24:09 -0500

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  // backend "azurerm" {
+  //   resource_group_name  = "tf_state"
+  //   storage_account_name = "tfstateTest"
+  //   container_name       = "tfstate"
+  //   key                  = "terraform.tfstate"
+  // }
+}
