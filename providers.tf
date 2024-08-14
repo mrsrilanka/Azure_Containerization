85b10213f27d04890d62413d31abc969e3fd33ce
Naninga Karunaratne <naninga.karu@gmail.com>
2024-08-14 16:36:11 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,15 @@
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
   }
