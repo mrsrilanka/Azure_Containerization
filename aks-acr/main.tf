0e5ac55b9acc0646493f6fafce448089e20eda9d
Naninga Karunaratne <naninga.karu@gmail.com>
2024-08-29 11:44:18 -0500

    Update main.tf

--- aks-acr/main.tf
+++ aks-acr/main.tf
@@ -3,12 +3,12 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_role_assignment" "role_acrpull" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-  skip_service_principal_aad_check = true
-}
+#resource "azurerm_role_assignment" "role_acrpull" {
+#  scope                            = azurerm_container_registry.acr.id
+#  role_definition_name             = "AcrPull"
+#  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+#  skip_service_principal_aad_check = true
+#}
 
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
@@ -39,7 +39,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   network_profile {
-    load_balancer_sku = "Standard"
+    load_balancer_sku = "standard"
     network_plugin    = "kubenet" # CNI
   }
 }
